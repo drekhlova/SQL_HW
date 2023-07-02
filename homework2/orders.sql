a2c7e958b0eb38774459c718fce31835f72c762f
Daria Rekhlova <dolly1993@yandex.ru>
2023-07-02 21:29:59 +0400

    Уточнили orders.sql

--- homework2/orders.sql
+++ homework2/orders.sql
@@ -25,5 +25,5 @@ CASE
 WHEN order_status = "OPEN" THEN "Order is in open state"
 WHEN order_status = "CLOSED" THEN "Order is closed"
 WHEN order_status = "CANCELLED" THEN "Order is cancelled"
-END AS full_order_status
+END full_order_status
 FROM orders;
